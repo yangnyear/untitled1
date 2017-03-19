224f86b4a97f5b816c12974fad8f4c28851dcfb2
羊荣毅_L <yangrongyi_em@163.com>
2017-03-19 16:21:26 +0800

    Joseplus

---
+++ src/linkedlist/DoubleList.java
@@ -0,0 +1,119 @@
+package linkedlist;
+
+import java.awt.*;
+
+/**
+ * Created by 羊荣毅_L on 2017/3/18.
+ * s双向链表
+ */
+public class DoubleList {
+    public static void main(String[] args) {
+        CycLink cyclink = new CycLink();
+        cyclink.setLen(5);
+        cyclink.createLink();
+        cyclink.setK(2);
+        cyclink.setM(2);
+        cyclink.show();
+        cyclink.play();
+    }
+}
+
+class Child {
+    int no;
+    Child nextChild = null;
+
+    public Child(int no) {
+        //给一个编号
+        this.no = no;
+    }
+}
+
+//环形链表
+class CycLink {
+    //先定义一个指向链表第一个小孩的引用
+    Child firstChild = null;
+    Child temp = null;
+    int len = 0;//表示共有几个小孩
+    int k = 0;
+    int m = 0;
+
+    //设置列表的长度
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    //设置从第几个人开始数数
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    //设置数几下
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    //开始play
+    public void play() {
+        Child temp = this.firstChild;
+
+        //1.先找到开始数数的人
+        for (int i = 1; i < k; i++) {
+            temp = temp.nextChild;
+        }
+        while (this.len != 1) {
+            //2.数m下
+            for (int j = 1; j < m; j++) {
+                temp = temp.nextChild;
+            }
+            //找到要出圈的前一个小孩
+            Child temp2 = temp;
+            while (temp2.nextChild != temp) {
+                temp2 = temp2.nextChild;
+            }
+            //3.将数到m的小孩，退出圈
+            temp2.nextChild = temp.nextChild;
+            //让temp指向下一个数数的小孩
+            temp = temp.nextChild;
+//    this.show();
+            this.len--;
+        }
+        //最后一个小孩
+        System.out.println("最后出圈：" + temp.no);
+    }
+
+    //初始化环形链表
+    public void createLink() {
+        for (int i = 1; i <= len; i++) {
+            if (i == 1) {
+                //创建第一个小孩
+                Child ch = new Child(i);
+                this.firstChild = ch;
+                this.temp = ch;
+            } else {
+                //创建最后一个孩子
+                if (i == len) {
+                    //继续创建孩子
+                    Child ch = new Child(i);
+                    temp.nextChild = ch;
+                    temp = ch;
+                    temp.nextChild = this.firstChild;
+                } else {
+                    //继续创建孩子
+                    Child ch = new Child(i);
+                    temp.nextChild = ch;
+                    temp = ch;
+                }
+            }
+        }
+    }
+
+    //打印该环形链表
+    public void show() {
+        //定义一个跑龙套
+        Child temp = this.firstChild;
+        do {
+            System.out.print(temp.no + " ");
+            temp = temp.nextChild;
+        } while (temp != this.firstChild);
+    }
+}
--- src/linkedlist/MyList.java
+++ src/linkedlist/MyList.java
@@ -1,15 +1,8 @@
 package linkedlist;
 
-
-import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
-import com.sun.org.apache.bcel.internal.generic.NEW;
-import treetext.Node;
-
-import java.util.LinkedList;
-import java.util.concurrent.ForkJoinPool;
-
 /**
  * Created by 羊荣毅_L on 2017/3/18.
+ * 学习链表
  */
 public class MyList {
 
@@ -74,36 +67,36 @@ public class MyList {
     public static void intersection(MyList listA, MyList listB) {
         System.out.println("开始");
         Node a = listA.root;
-        Node b=listB.root;
-        int maxOfA=0,maxofB=0;
-        int j=0,k=0;
-        int[] valusesOfA=new int[listA.size];
-        int[] valusesOfB=new int[listB.size];
+        Node b = listB.root;
+        int maxOfA = 0, maxofB = 0;
+        int j = 0, k = 0;
+        int[] valusesOfA = new int[listA.size];
+        int[] valusesOfB = new int[listB.size];
         while (a != null) {
-            valusesOfA[j]=a.val;
+            valusesOfA[j] = a.val;
             j++;
-            if (a.next==null) maxOfA=a.val;
+            if (a.next == null) maxOfA = a.val;
             a = a.next;
 
         }
         System.out.println("a完成");
         while (b != null) {
-            valusesOfB[k]=b.val;
+            valusesOfB[k] = b.val;
             k++;
-            if (b.next==null) maxofB=b.val;
+            if (b.next == null) maxofB = b.val;
             b = b.next;
 
         }
         System.out.println("b完成");
-        int max=maxOfA>maxofB? maxOfA:maxofB;
-        int[] attr=new int[max];
+        int max = maxOfA > maxofB ? maxOfA : maxofB;
+        int[] attr = new int[max];
         for (int i = 0; i < max; i++) {
-           attr[i]=0;
+            attr[i] = 0;
             System.out.print(attr[i]);
         }
         System.out.println("数组创建完成");
         a = listA.root;
-        b=listB.root;
+        b = listB.root;
 //        while (a!=null){
 //            while (b!=null){
 //                if (a.val== b.val){
@@ -113,14 +106,14 @@ public class MyList {
 //        }
         for (int i = 0; i < valusesOfA.length; i++) {
             for (int l = 0; l < valusesOfB.length; l++) {
-                if (valusesOfA[i]==valusesOfB[l])
-                    attr[valusesOfA[i]]=1;
+                if (valusesOfA[i] == valusesOfB[l])
+                    attr[valusesOfA[i]] = 1;
             }
         }
         System.out.println("交集是:");
         for (int i = 0; i < max; i++) {
-            if (attr[i]==1){
-                System.out.print(" "+i);
+            if (attr[i] == 1) {
+                System.out.print(" " + i);
             }
         }
 
@@ -131,14 +124,14 @@ public class MyList {
         MyList list2 = new MyList();
         for (int i = 0; i < 10; i++) {
             list.put(i);
-            list2.put(i+5);
+            list2.put(i + 5);
         }
         list.printAll();
         list.insert(2, 666);
         list.printAll();
         System.out.println(list.contans(12));
         System.out.println(list.contans(3));
-        intersection(list,list2);
+        intersection(list, list2);
     }
 
 }
