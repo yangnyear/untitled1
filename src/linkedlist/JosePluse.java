f6725a47e9372a982d0dd56eb5aa2b19651a31b3
羊荣毅_L <yangrongyi_em@163.com>
2017-03-19 20:01:15 +0800

    Joseplus2

--- src/linkedlist/JosePluse.java
+++ src/linkedlist/JosePluse.java
@@ -20,39 +20,39 @@ public class JosePluse {
         ListIterator<Integer> iter = L.listIterator();
         Integer item=0;
         numLeft = n;
-        mPrime = m % n;
+        mPrime = m % n;//找到要出去的那个child
         for (i=0; i<n; i++)
         {
-            mPrime = m % numLeft;
-            if (mPrime <= numLeft/2)
+            mPrime = m % numLeft;//重新计算要出去的child
+            if (mPrime <= numLeft/2)//二分,当要找的那个在前一半时
             {
                 if (iter.hasNext())
                     item = iter.next();
                 for (j=0; j<mPrime; j++)
                 {
                     if (!iter.hasNext())
-                        iter = L.listIterator();
+                        iter = L.listIterator();//从头开始循环到要出去的那个child
                     item = iter.next();
                 }
             }
-            else
+            else//如果在后一半
             {
                 for (j=0; j<numLeft-mPrime; j++)
                 {
                     if (!iter.hasPrevious())
-                        iter = L.listIterator(L.size());
+                        iter = L.listIterator(L.size());//从尾部开始循环到要出去的那个child
                     item = iter.previous();
                 }
             }
             System.out.print("Removed " + item + " ");
-            iter.remove();
+            iter.remove();//将该child移除
             if (!iter.hasNext())
-                iter = L.listIterator();
+                iter = L.listIterator();//重新初始化
             System.out.println();
             for (Integer x:L)
                 System.out.print(x + " ");
             System.out.println();
-            numLeft--;
+            numLeft--;//改变总人数
         }
         System.out.println();
     }
